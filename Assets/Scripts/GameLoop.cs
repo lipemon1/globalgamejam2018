3d2dce0407745842cbd6f1e2cb8455bca144d531
gustavopsantos <gustavopereira_ppta@hotmail.com>
2018-01-27 15:37:05 -0200

    PlayerSelection UI started, Nameplates done

--- Assets/Scripts/GameLoop.cs
+++ Assets/Scripts/GameLoop.cs
@@ -5,38 +5,31 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using XInputDotNetPure;
 
-public class GameLoop : MonoBehaviour {
-
+public class GameLoop : MonoBehaviour
+{
     public static GameLoop Instance;
 
     public const int MAX_ENERGY = 4;
 
-    [Header("General Settings")]
-    [SerializeField] private float _startDelay = 3f;
+    [Header("General Settings")] [SerializeField] private float _startDelay = 3f;
     [SerializeField] private float _endDelay = 3f;
     [SerializeField] private Text _messageText;
 
-    [Header("Players Controllers")]
-    [SerializeField] private List<Player> _playersControllersList = new List<Player>();
+    [Header("Players Controllers")] [SerializeField] private List<Player> _playersControllersList = new List<Player>();
 
-    [Header("Player Config")]
-    [SerializeField] private Material[] _playerMaterials;
+    [Header("Player Config")] [SerializeField] private Material[] _playerMaterials;
     [SerializeField] private GameObject _playerPrefab;
     [SerializeField] private int _initialEnergy;
     [SerializeField] private Transform[] _spawnTransforms = new Transform[12];
 
-    [Header("Other Info")]
-    [HideInInspector]
-    private WaitForSeconds _startWait;
+    [Header("Other Info")] [HideInInspector] private WaitForSeconds _startWait;
     [HideInInspector] private WaitForSeconds _endWait;
     [HideInInspector] private float _timeAlive;
 
-    [Header("Messages")]
-    [SerializeField] private string _startMessage = "Começando...";
+    [Header("Messages")] [SerializeField] private string _startMessage = "Começando...";
     [SerializeField] private string _finalMessage = "Terminando...";
 
-    [Header("Tickets Available")]
-    [SerializeField] private int _currentDeathTickets = 0;
+    [Header("Tickets Available")] [SerializeField] private int _currentDeathTickets = 0;
     [SerializeField] private int _deathTicketsForMatch = 20;
 
     private void Awake()
@@ -125,7 +118,7 @@ public class GameLoop : MonoBehaviour {
 
     private void ForceManualSettingPlayers()
     {
-        if(Global.Player.Count == 0)
+        if (Global.Player.Count == 0)
         {
             Global.ResetAllPlayers();
             for (int i = 0; i < 4; i++) Global.Player[i].exist = true;
@@ -135,15 +128,25 @@ public class GameLoop : MonoBehaviour {
     // This function is used to turn all the tanks back on and reset their positions and properties.
     private void ResetAllPlayers()
     {
+        foreach (var userPlate in NamePlateManager.Instance.UserPlates)
+            userPlate.DisableNamePlate();
+
         for (int i = 0; i < Global.MaxPlayers; i++)
         {
             if (Global.Player[i].exist)
             {
-                Global.Player[i].Instance = Instantiate(_playerPrefab, _spawnTransforms[i].position, _spawnTransforms[i].rotation).GetComponent<Player>();
+                Global.Player[i].Instance = Instantiate(_playerPrefab, _spawnTransforms[i].position, _spawnTransforms[i].rotation)
+                    .GetComponent<Player>();
                 Global.Player[i].PlayerEnergy = Global.Player[i].Instance.gameObject.GetComponent<EnergyHandler>();
                 Global.Player[i].Instance.gameObject.GetComponent<EnergyHandler>().RecieveSomeEnergy(_initialEnergy);
                 Global.Player[i].Instance.Index = (PlayerIndex) i;
                 Global.Player[i].Instance.GetComponent<Renderer>().material = _playerMaterials[i];
+
+                foreach (var userPlate in NamePlateManager.Instance.UserPlates)
+                    if (userPlate.Index == Global.Player[i].Instance.Index)
+                    {
+                        userPlate.Owner = Global.Player[i].Instance;
+                    }
             }
         }
     }
@@ -180,7 +183,7 @@ public class GameLoop : MonoBehaviour {
     {
         // By default when a round ends there are no winners so the default end message is a draw.
         string message = "JOGO ACABOU";
-        
+
 
         return message;
     }
