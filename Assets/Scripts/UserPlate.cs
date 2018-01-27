1e784efa79545ed781c856687abe0c33fecbdb88
Ramon Ferreira <ramon_felipe@outlook.com>
2018-01-27 14:10:31 -0200

    Importing canvas to the game scene

---
+++ Assets/Scripts/UserPlate.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UserPlate : MonoBehaviour
+{
+    [SerializeField] private Image _fillImage;
+
+    public void SetFill(float value) { _fillImage.fillAmount = value; }
+}
