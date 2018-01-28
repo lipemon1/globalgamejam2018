bdfa0549bfb12679b37a9882d2b7a02842ccc013
Lucas Straub <lucas.str@live.com>
2018-01-28 02:56:38 -0200

    More design at the pre match scene

--- Assets/Scripts/BulletBehaviour.cs
+++ Assets/Scripts/BulletBehaviour.cs
@@ -53,7 +53,7 @@ public class BulletBehaviour : MonoBehaviour
             }
             else
             {
-                transform.Translate(direction * _currentSpeed * Time.deltaTime, Space.World);
+                transform.Translate(direction * _currentSpeed * Time.deltaTime, Space.World); 
                 //Rigidbody rb = GetComponent<Rigidbody>();
                 //rb.velocity = (_currentSpeed * direction * Time.deltaTime + transform.position) - transform.position;
             }
