1e397d928dc99da0cafc4022fd2122451492921e
Lucas Straub <lucas.str@live.com>
2018-01-28 13:50:32 -0200

    Teleports

---
+++ Assets/Teleport.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour {
+
+    public Vector3 teleport;
+    public bool draw;
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.tag == "Player")
+            col.transform.Translate(teleport, Space.World);
+        //col.transform.position = teleport;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (draw)
+            Gizmos.DrawWireCube(transform.position + teleport, transform.localScale);
+    }
+}
