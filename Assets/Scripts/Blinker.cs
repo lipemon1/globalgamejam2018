c5df72b565db6c5f121e6a03ae4e8fc4542c5ec7
Ramon Ferreira <ramon_felipe@outlook.com>
2018-01-27 22:08:44 -0200

    changing blinking script to use animation curve and emissive material

--- Assets/Scripts/Blinker.cs
+++ Assets/Scripts/Blinker.cs
@@ -1,45 +1,63 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Blinker : MonoBehaviour {
-
+public class Blinker : MonoBehaviour
+{
     [Header("Debug")]
-    [SerializeField] private bool _isBlinking;
+    [SerializeField]
+    private bool _isBlinking;
 
     [Header("Object to Blink")]
-    [SerializeField] private GameObject _target;
-    [Range(1, 10)]
-    [SerializeField] private float _blinkTimes = 5;
-    [SerializeField] private float _delayBetweenBlinks;
+    [SerializeField]
+    private Renderer[] _target;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [Header("Blink Settings")]
+    public Color BlinkColor = Color.yellow;
+    [SerializeField] [Tooltip("Animation Curve need to be in Ping Pong mode")]
+    private AnimationCurve _blinkCurve;
+    [SerializeField] private float _blinkSpeed = 1;
+    [SerializeField] private float _blinkDuration = 1;
+
+
+    //public void StartBlink(System.Action actionAfterBlink)
+    //{
+    //    StartCoroutine(BlinkBehaviour(_blinkTimes, _delayBetweenBlinks, actionAfterBlink));
+    //}
+
+    private void Update()
+    {
+        if (_isBlinking)
+        {
+            var value = _blinkCurve.Evaluate(Time.time * _blinkSpeed);
+            Color color = BlinkColor * value;
+            foreach (var rend in _target) rend.material.SetColor("_EmissionColor", color);
+        }
+    }
 
     public void StartBlink(System.Action actionAfterBlink)
     {
-        StartCoroutine(BlinkBehaviour(_blinkTimes, _delayBetweenBlinks, actionAfterBlink));
-    }
-
-    IEnumerator BlinkBehaviour(float blinkTimes, float delay, System.Action actionAfterBlink)
-    {
         _isBlinking = true;
-
-        while(blinkTimes > 0)
-        {
-            _target.SetActive(!_target.activeInHierarchy);
-            blinkTimes--;
-            yield return new WaitForSeconds(delay);
-        }
-
-        _isBlinking = false;
-        actionAfterBlink();
+        Invoke("StopBlinking", _blinkDuration);
     }
+
+    private void StopBlinking()
+    {
+        _isBlinking = false;
+        foreach (var rend in _target) rend.material.SetColor("_EmissionColor", Color.black);
+    }
+
+    //IEnumerator BlinkBehaviour(float blinkTimes, float delay, System.Action actionAfterBlink)
+    //{
+    //    _isBlinking = true;
+
+    //    while (blinkTimes > 0)
+    //    {
+    //        _target.SetActive(!_target.activeInHierarchy);
+    //        blinkTimes--;
+    //        yield return new WaitForSeconds(delay);
+    //    }
+
+    //    _isBlinking = false;
+    //    actionAfterBlink();
+    //}
 }
